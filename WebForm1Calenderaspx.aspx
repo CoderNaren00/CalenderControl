97db6b74dbbaeaf7face7e49150faf25ef0288a7
_Coder_Naren <narendrajalamkar8399@gmail.com>
2024-08-09 22:43:12 +0530

    Add project files.

---
+++ WebForm1Calenderaspx.aspx
@@ -0,0 +1,21 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1Calenderaspx.aspx.cs" Inherits="CalenderControl.WebForm1Calenderaspx" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+        </div>
+        <asp:Calendar ID="Calendar1" runat="server" OnSelectionChanged="Calendar1_SelectionChanged"></asp:Calendar>
+        <br />
+        <br />
+        <asp:TextBox ID="TextBox1" runat="server" TextMode="Date"></asp:TextBox>
+        <br />
+        <asp:Button ID="btnSubmit" runat="server" OnClick="btnSubmit_Click" Text="Submit" />
+    </form>
+</body>
+</html>
---
+++ WebForm1Calenderaspx.aspx.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace CalenderControl
+{
+    public partial class WebForm1Calenderaspx : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            //hiding Calendar control
+            Calendar1.Visible = false;
+        }
+
+
+        protected void Calendar1_SelectionChanged(object sender, EventArgs e)
+        {
+            TextBox1.Text = Calendar1.SelectedDate.ToString();
+            Calendar1.Visible = false; //hiding the calendar
+        }
+        protected void btnSubmit_Click(object sender, EventArgs e)
+        {
+            //showing the calendar to user
+            Calendar1.Visible = true;
+        }
+    }
+}
